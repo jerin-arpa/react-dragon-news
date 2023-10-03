1784ec13e5842159f1713be273b86bf833f5c4c6
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-04 00:01:06 +0600

    Date time formatting using moment.js

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h2 className="text-3xl font-poppins">Home</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="border md:col-span-2">
+                    <h2 className="text-4xl">News coming</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,7 +1,13 @@
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
+
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-3xl">Header</h2>
+        <div className='my-10 text-center'>
+            <div className='flex justify-center mb-4'>
+                <img src={logo} alt="" /></div>
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };

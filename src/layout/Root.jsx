5fa41ec0df86ad13ec12ad1def3a913f42015e36
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-03 22:42:48 +0600

    Custom tailwind class and font setup

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
-        <div className="container mx-auto px-5">
+        <div className="container mx-auto px-5 font-poppins">
             <Outlet></Outlet>
         </div>
     );

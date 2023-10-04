431edd39615412a3fa629cdd021e42f43ec49b8f
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-04 20:32:06 +0600

    create auth context and integrate register

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
+
     return (
         <div>
             <Navbar></Navbar>
 
             <div className="flex justify-center mt-14 ">
-                <div className="bg-slate-200 rounded-lg w-full md:w-3/4 lg:w-1/2 p-8">
-                    <h2 className="text-3xl font-bold text-center pt-7">Login Now!!</h2>
+                <div className="bg-gray-100 rounded-lg w-full md:w-3/4 lg:w-1/2 p-8">
+                    <h2 className="text-3xl font-bold text-center pt-7">Login your account</h2>
                     <div className="flex justify-center">
                         <hr className="bg-black h-1 mt-6 w-1/2" />
                     </div>
-                    <form className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -31,11 +40,12 @@ const Login = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-neutral">Login</button>
                         </div>
-
-                        <div className="flex justify-center mt-5">
-                            <h2>Dont have an account? <Link to='/signup' className="text-red-500 underline font-bold">SignUp</Link></h2>
-                        </div>
                     </form>
+
+
+                    <div className="flex justify-center mb-4">
+                        <h2>Do not have an account? <Link to='/signup' className="text-red-500 underline font-bold">SignUp</Link></h2>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,18 +1,44 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const SignUp = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+    const handleSignUp = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name, photo, email, password);
+
+
+        // create user
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
     return (
         <div>
             <Navbar></Navbar>
 
             <div className="flex justify-center mt-14 ">
-                <div className="bg-slate-200 rounded-lg w-full md:w-3/4 lg:w-1/2 p-8">
-                    <h2 className="text-3xl font-bold text-center pt-7">SignUp Now</h2>
+                <div className="bg-gray-100 rounded-lg w-full md:w-3/4 lg:w-1/2 p-8">
+                    <h2 className="text-3xl font-bold text-center pt-7">SignUp your account</h2>
                     <div className="flex justify-center">
                         <hr className="bg-black h-1 mt-6 w-1/2" />
                     </div>
-                    <form className="card-body">
+                    <form onSubmit={handleSignUp} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Your Name</span>
@@ -41,7 +67,7 @@ const SignUp = () => {
                             <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
                         </div>
 
-                        <div className="flex gap-3">
+                        <div className="flex gap-3 mt-5">
                             <input type="checkbox" name="" id="" />
                             <p>Accept Terms & Conditions</p>
                         </div>
@@ -49,11 +75,11 @@ const SignUp = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-neutral">Login</button>
                         </div>
-
-                        <div className="flex justify-center mt-5">
-                            <h2>Already have an account? <Link to='/login' className="text-red-500 underline font-bold">Login</Link></h2>
-                        </div>
                     </form>
+
+                    <div className="flex justify-center mb-4">
+                        <h2>Already have an account? <Link to='/login' className="text-red-500 underline font-bold">Login</Link></h2>
+                    </div>
                 </div>
             </div>
         </div>
